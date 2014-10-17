250c45be4ca1ff4e1010744bcb55c49441d256a9
Chan <chandler14362@gmail.com>
2014-10-16 23:45:47 -0400

    Cleaned up PirateInventory

--- pirates/uberdog/PirateInventory.py
+++ pirates/uberdog/PirateInventory.py
@@ -1,13 +1,8 @@
-# File: P (Python 2.4)
-
-from direct.distributed.DistributedObject import DistributedObject
 from pirates.uberdog.DistributedInventory import DistributedInventory
 from direct.directnotify.DirectNotifyGlobal import directNotify
 
+
 class PirateInventory(DistributedInventory):
     notify = directNotify.newCategory('Inventory')
-    
-    def __init__(self, repository):
-        DistributedInventory.__init__(self, repository)
 
 
